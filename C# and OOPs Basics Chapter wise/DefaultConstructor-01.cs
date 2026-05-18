e881726f9df5b50ad91065306c75f688ffb1136c
Muhammad Abrar ul haq <ulhaqabrar106@gmail.com>
2026-05-18 17:07:41 +0500

    Add project files.

---
+++ C# and OOPs Basics Chapter wise/DefaultConstructor-01.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__and_OOPs_Basics_Chapter_wise
+{
+    internal class DefaultConstructor_01
+    {
+            class Student
+        {
+            public string name;
+            public int age;
+            public Student()
+            {
+                name = "Abrar";
+                age = 20;
+            }
+            public void Display()
+            {
+                Console.WriteLine(name + " " + age);
+            }
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Student s1 = new Student();
+
+                s1.Display();
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
