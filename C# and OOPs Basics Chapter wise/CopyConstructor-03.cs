0901a2e629951b42e3d29daf8451de7980af1b75
Muhammad Abrar ul haq <ulhaqabrar106@gmail.com>
2026-05-23 10:06:27 +0500

    Copy Constructor in C Sharp

--- C# and OOPs Basics Chapter wise/CopyConstructor-03.cs
+++ C# and OOPs Basics Chapter wise/CopyConstructor-03.cs
@@ -1,5 +1,4 @@
-﻿/*
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,32 +6,41 @@ using System.Threading.Tasks;
 
 namespace C__and_OOPs_Basics_Chapter_wise
 {
-    internal class ParameterizedConstructor_02
+    internal class CopyConstructor_03
     {
         class Student
         {
             public string name;
             public int age;
+
             public Student(string n, int a)
             {
                 name = n;
                 age = a;
             }
 
+            public string Name { get => name; set => name = value; }
+            public int Age { get => age; set => age = value; }
+
+            public Student(Student s)
+            {
+                name = s.name;
+                age = s.age;
+            }
             public void Display()
             {
                 Console.WriteLine($"Name: {name}, Age: {age}");
             }
         }
+
         class Program
         {
             static void Main(String[] args)
             {
                 Student s1 = new Student("Abrar", 23);
-                s1.Display();
+                Student s2 = new Student(s1);
+                s2.Display();
             }
         }
     }
 }
-
- */
