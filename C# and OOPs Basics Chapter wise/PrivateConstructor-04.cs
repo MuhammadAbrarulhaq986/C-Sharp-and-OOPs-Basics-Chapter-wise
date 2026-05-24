25cc610462f551fcf531781c7a9955b1015c49a5
Muhammad Abrar ul haq <ulhaqabrar106@gmail.com>
2026-05-24 16:20:18 +0500

    Private Constructor in C#

---
+++ C# and OOPs Basics Chapter wise/PrivateConstructor-04.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__and_OOPs_Basics_Chapter_wise
+{
+    internal class PrivateConstructor
+    {
+        class Singleton
+        {
+            private static Singleton instance;
+
+            private Singleton() 
+            {
+                Console.WriteLine("Singleton instance created.");
+            }
+            public static Singleton GetInstance() 
+            {
+                if ( instance == null)
+                {
+                    instance = new Singleton();
+                }
+                return instance;
+            }
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Singleton s1 = Singleton.GetInstance(); 
+            }
+        }
+    }
+}
