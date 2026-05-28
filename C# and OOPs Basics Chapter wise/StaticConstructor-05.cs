a98fcc40f4470c22fa057bfe5540816f57bfe843
Muhammad Abrar ul haq <ulhaqabrar106@gmail.com>
2026-05-28 16:25:04 +0500

    Static Constructor Done

---
+++ C# and OOPs Basics Chapter wise/StaticConstructor-05.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__and_OOPs_Basics_Chapter_wise
+{
+    internal class StaticConstructor_05
+    {
+        class Company
+        {
+            public static string companyName;
+            static Company()
+            {
+                companyName = "BhaiJan Corporation";
+                Console.WriteLine("Static Constructor Called");
+            }
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Company Name: " + Company.companyName);
+            }
+        }
+    }
+}
