4e5045c3c0c1fd76e40d20ae0b2827d56372cf5d
Muhammad Abrar ul haq <ulhaqabrar106@gmail.com>
2026-05-29 15:43:43 +0500

    Constructor Overloading in C# done

---
+++ C# and OOPs Basics Chapter wise/ConstructorOverloading-06.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__and_OOPs_Basics_Chapter_wise
+{
+    internal class ConstructorOverloading_06
+    {
+        class ClassName
+        {
+            // Default constructor
+            public ClassName()
+            {
+                Console.WriteLine("Default Constructor Called!");
+            }
+            // Parameterized constructor
+            public ClassName(int x)
+            {
+                Console.WriteLine($"Parameterized Constructor Called with x = {x}");
+            }
+            // Another overloaded constructor
+            public ClassName(int x, int y)
+            {
+                Console.WriteLine($"Overloaded Constructor Called with x = {x} and y = {y}");
+            }
+        }
+    class Program
+    {
+            static void Main(String[] args)
+        {
+            // Demonstrating the different ways objects can be initialized
+            ClassName obj1 = new ClassName();
+            ClassName obj2 = new ClassName(10);
+            ClassName obj3 = new ClassName(10, 20);
+        }
+    }
+}
+}
