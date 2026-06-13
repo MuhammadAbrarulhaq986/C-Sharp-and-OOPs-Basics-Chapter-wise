180cd277a19536357fa94566efdda21991075c58
Muhammad Abrar ul haq <ulhaqabrar106@gmail.com>
2026-06-13 10:53:31 +0500

    Hybrid Inheritance chapter  done in C#

--- C# and OOPs Basics Chapter wise/11_HierarchicalInheritance.cs
+++ C# and OOPs Basics Chapter wise/11_HierarchicalInheritance.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿/*
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -55,4 +56,4 @@ namespace C__and_OOPs_Basics_Chapter_wise
         }
     }
 }
-
+ */
---
+++ C# and OOPs Basics Chapter wise/12_HybridInheritance.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__and_OOPs_Basics_Chapter_wise
+{
+    internal class _12_HybridInheritance
+    {
+        // 1. Base Class
+        class Vehicle
+        {
+            public void Move()
+            {
+                Console.WriteLine("This vehicle is moving forward.");
+            }
+        }
+
+        // 2. Interface to enable hybrid capability without breaking C# rules
+        interface IElectric
+        {
+            void ChargeBattery();
+        }
+
+        // 3. Derived Class implementing Hybrid Inheritance
+        // It inherits from one class (Vehicle) and implements one interface (IElectric)
+        class ElectricCar : Vehicle, IElectric
+        {
+            public void ChargeBattery()
+            {
+                Console.WriteLine("The battery is charging... 100%!");
+            }
+
+            public void DriveSilent()
+            {
+                Console.WriteLine("Driving smoothly and silently.");
+            }
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                // Create an object of the hybrid class
+                ElectricCar myTesla = new ElectricCar();
+
+                // 1. Calling method from the Parent Class
+                myTesla.Move();
+
+                // 2. Calling method from the Interface
+                myTesla.ChargeBattery();
+
+                // 3. Calling method from its own Class
+                myTesla.DriveSilent();
+
+                // Keeps the console window open
+                Console.ReadLine();
+            }
+        }
+    }
+}
