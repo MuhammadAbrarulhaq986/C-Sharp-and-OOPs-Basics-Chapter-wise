6661444e313ee07ff489b9786e8dc8b84466c347
Muhammad Abrar ul haq <ulhaqabrar106@gmail.com>
2026-06-14 15:34:54 +0500

    Multiple Inheritance done  in C#

---
+++ C# and OOPs Basics Chapter wise/13_MultipleInheritance.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__and_OOPs_Basics_Chapter_wise
+{
+    internal class _13_MultipleInheritance
+    {
+        // First Interface
+        interface ICamera
+        {
+            void TakePhoto();
+        }
+
+        // Second Interface
+        interface IPhone
+        {
+            void MakeCall();
+        }
+
+        // Child Class implementing both interfaces
+        class SmartPhone : ICamera, IPhone
+        {
+            public void TakePhoto()
+            {
+                Console.WriteLine("Photo captured successfully!");
+            }
+
+            public void MakeCall()
+            {
+                Console.WriteLine("Dialing number and placing a call...");
+            }
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                // Create an object of the class that implements multiple behaviors
+                SmartPhone myPhone = new SmartPhone();
+
+                // Accessing methods from both interfaces
+                myPhone.MakeCall();
+                myPhone.TakePhoto();
+
+                // Keeps the console window open
+                Console.ReadLine();
+            }
+        }
+    }
+}
