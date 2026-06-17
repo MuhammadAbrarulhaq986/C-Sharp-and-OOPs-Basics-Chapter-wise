4da965ad7cf72eb8eecc0482e9ed0abbf1400cc6
Muhammad Abrar ul haq <ulhaqabrar106@gmail.com>
2026-06-17 22:30:16 +0500

    Public Access Modifier in C# done

--- C# and OOPs Basics Chapter wise/13_MultipleInheritance.cs
+++ C# and OOPs Basics Chapter wise/13_MultipleInheritance.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿/*
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -51,3 +52,5 @@ namespace C__and_OOPs_Basics_Chapter_wise
         }
     }
 }
+
+ */
---
+++ C# and OOPs Basics Chapter wise/14_PublicAccessModifier.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__and_OOPs_Basics_Chapter_wise
+{
+    internal class _14_PublicAccessModifier
+    {
+        public class PublicParent
+        {
+            public string publicMessage = "Hello from Public! (Accessible everywhere)";
+        }
+
+        public class PublicChild : PublicParent
+        {
+            public void DisplayMessage()
+            {
+                // ✅ Allowed: Child class can fully access parent's public fields
+                Console.WriteLine(publicMessage);
+            }
+        }
+
+        class Program
+        {
+            static void Main()
+            {
+                PublicChild childObj = new PublicChild();
+                childObj.DisplayMessage();
+
+                // ✅ Allowed: External code can access public fields directly
+                Console.WriteLine(childObj.publicMessage);
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
+
