96424569bc057e1fdc8ac3dd030208ae6c6a8b2e
Muhammad Abrar ul haq <ulhaqabrar106@gmail.com>
2026-06-20 09:11:39 +0500

    Private Access Modifier in C# done

--- C# and OOPs Basics Chapter wise/15_ProtectedAccessModifier.cs
+++ C# and OOPs Basics Chapter wise/15_ProtectedAccessModifier.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿/*
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -38,3 +39,4 @@ namespace C__and_OOPs_Basics_Chapter_wise
     }
 }
 
+ */
---
+++ C# and OOPs Basics Chapter wise/16_PrivateAccessModifier.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__and_OOPs_Basics_Chapter_wise
+{
+    internal class _16_PrivateAccessModifier
+    {
+    
+        public class PrivateParent
+        {
+            private string privateMessage = "Hello from Private! (Hidden completely)";
+        }
+
+        public class PrivateChild : PrivateParent
+        {
+            public void DisplayMessage()
+            {
+                // ❌ Error if uncommented: Private fields are not visible to child classes
+                // Console.WriteLine(privateMessage); 
+
+                Console.WriteLine("Private members cannot be accessed directly by child classes.");
+            }
+        }
+
+        class Program
+        {
+            static void Main()
+            {
+                PrivateChild childObj = new PrivateChild();
+                childObj.DisplayMessage();
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
