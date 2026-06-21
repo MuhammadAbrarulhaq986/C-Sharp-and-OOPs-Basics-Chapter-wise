6da806f4e10c04467d8e0533ca8b806928b8d912
Muhammad Abrar ul haq <ulhaqabrar106@gmail.com>
2026-06-22 00:15:35 +0500

    Protected Internal Access Modifier Done in C#

---
+++ C# and OOPs Basics Chapter wise/18_ProtectedInternalAccessModifier.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__and_OOPs_Basics_Chapter_wise
+{
+    public class ProtectedInternalParent
+    {
+        protected internal string protectedInternalMessage = "Hello from Protected Internal! (Same project OR sub-project children)";
+    }
+
+    public class ProtectedInternalChild : ProtectedInternalParent
+    {
+        public void DisplayMessage()
+        {
+            // ✅ Allowed: Child can access it seamlessly
+            Console.WriteLine(protectedInternalMessage);
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            ProtectedInternalChild childObj = new ProtectedInternalChild();
+            childObj.DisplayMessage();
+
+            // ✅ Allowed: Acts like internal within the same project assembly
+            Console.WriteLine(childObj.protectedInternalMessage);
+
+            Console.ReadLine();
+        }
+    }
+}
+
