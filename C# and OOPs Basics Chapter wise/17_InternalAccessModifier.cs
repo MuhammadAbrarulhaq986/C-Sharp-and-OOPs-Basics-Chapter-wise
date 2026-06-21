36c025ca282e537a812ba3dd45266f4a901a212a
Muhammad Abrar ul haq <ulhaqabrar106@gmail.com>
2026-06-21 17:21:46 +0500

    Internal Access Modifier done in C#

---
+++ C# and OOPs Basics Chapter wise/17_InternalAccessModifier.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__and_OOPs_Basics_Chapter_wise
+{
+    internal class _17_InternalAccessModifier
+    {
+        public class InternalParent
+        {
+            internal string internalMessage = "Hello from Internal! (Accessible within the same project)";
+        }
+
+        public class InternalChild : InternalParent
+        {
+            public void DisplayMessage()
+            {
+                // ✅ Allowed: Accessible because parent and child sit in the same assembly project
+                Console.WriteLine(internalMessage);
+            }
+        }
+
+        class Program
+        {
+            static void Main()
+            {
+                InternalChild childObj = new InternalChild();
+                childObj.DisplayMessage();
+
+                // ✅ Allowed: External classes in the SAME project can read internal items
+                Console.WriteLine(childObj.internalMessage);
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
+
