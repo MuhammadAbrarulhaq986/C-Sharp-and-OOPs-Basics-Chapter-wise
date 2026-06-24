35f87035a11d20d6d2c736478547e5bd68d49dba
Muhammad Abrar ul haq <ulhaqabrar106@gmail.com>
2026-06-24 12:52:16 +0500

    Polymorphism: Method Overload Number in C# done

--- C# and OOPs Basics Chapter wise/20_PrivateProtectedAccessModifier.cs
+++ C# and OOPs Basics Chapter wise/20_PrivateProtectedAccessModifier.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿/*
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -37,4 +38,4 @@ namespace C__and_OOPs_Basics_Chapter_wise
         }
     }
 }
-
+ */
---
+++ C# and OOPs Basics Chapter wise/21_Polymorphism_MethodOverloadNumber.cs
@@ -0,0 +1,43 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__and_OOPs_Basics_Chapter_wise
+{
+    internal class _21_Polymorphism_MethodOverloadNumber 
+    {
+        class Program
+        {
+            // 1. Method with ONE integer parameter
+            void display(int a)
+            {
+                Console.WriteLine("Arguments: " + a);
+            }
+
+            // 2. Overloaded method with TWO integer parameters
+            void display(int a, int b)
+            {
+                Console.WriteLine("Arguments: " + a + " and " + b);
+            }
+
+            static void Main(string[] args)
+            {
+                // Creating class object to call instance methods
+                Program p1 = new Program();
+
+                // Calls the 1st method (takes 1 argument)
+                p1.display(100);
+
+                // Calls the 2nd method (takes 2 arguments)
+                p1.display(100, 200);
+
+                // Holds the console screen open
+                Console.ReadLine();
+            }
+        }
+    }
+}
+
