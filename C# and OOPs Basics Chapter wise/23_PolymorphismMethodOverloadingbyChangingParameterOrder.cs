514c2f5a348e56a9ee70e9abf1bcd64ed267aaf6
Muhammad Abrar ul haq <ulhaqabrar106@gmail.com>
2026-06-26 01:05:02 +0500

    Polymorphism: Method Overloading by Changing Parameter Order in C# done

---
+++ C# and OOPs Basics Chapter wise/23_PolymorphismMethodOverloadingbyChangingParameterOrder.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__and_OOPs_Basics_Chapter_wise
+{
+    internal class _23_PolymorphismMethodOverloadingbyChangingParameterOrder
+    {
+        class Program
+        {
+            // 1. Order: Int first, then String
+            void display(int a, string b)
+            {
+                Console.WriteLine("int: " + a);
+                Console.WriteLine("string: " + b);
+            }
+
+            // 2. Order: String first, then Int
+            void display(string b, int a)
+            {
+                Console.WriteLine("string: " + b);
+                Console.WriteLine("int: " + a);
+            }
+
+            static void Main(string[] args)
+            {
+                Program p1 = new Program();
+
+                Console.WriteLine("--- Calling First Mix ---");
+                // Matches method 1 (int, string)
+                p1.display(100, "Programming");
+
+                Console.WriteLine("\n--- Calling Second Mix ---");
+                // Matches method 2 (string, int)
+                p1.display("Programiz", 400);
+
+                // Holds the console screen open
+                Console.ReadLine();
+            }
+        }
+    }
+}
+
