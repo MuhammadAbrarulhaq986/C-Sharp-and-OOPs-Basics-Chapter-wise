dab5a8910d6317e22dcaf615e0bd4a71d58de750
Muhammad Abrar ul haq <ulhaqabrar106@gmail.com>
2026-06-29 22:31:51 +0500

    Sealed Class (Preventing Inheritance) done in C#

---
+++ C# and OOPs Basics Chapter wise/26_SealedClass(PreventingInheritance).cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__and_OOPs_Basics_Chapter_wise
+{
+    internal class _26_SealedClass_PreventingInheritance_
+    {
+        // Marked as sealed to strictly avoid further inheritance
+        public sealed class Animal
+        {
+            public void Eat()
+            {
+                Console.WriteLine("Eating...");
+            }
+        }
+
+        // ❌ COMPILATION ERROR: 'Dog': cannot derive from sealed type 'Animal'
+        // class Dog : Animal 
+        // {
+        // }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                // Note: Even though a sealed class cannot be inherited,
+                // we can still create objects and use its public members safely!
+                Animal myAnimal = new Animal();
+                myAnimal.Eat();
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
+
