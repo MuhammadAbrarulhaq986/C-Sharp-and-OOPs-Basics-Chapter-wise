3c22fdf1b69a6ffec380c8793a56202e3bcbb8ed
Muhammad Abrar ul haq <ulhaqabrar106@gmail.com>
2026-07-03 00:28:08 +0500

    Non-Generic Collections Hashtable done in C#

---
+++ C# and OOPs Basics Chapter wise/30_Non-GenericCollectionsHashtable.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__and_OOPs_Basics_Chapter_wise
+{
+    internal class _30_Non_GenericCollectionsHashtable
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                // Creating a Hashtable
+                Hashtable table = new Hashtable();
+
+                // Adding Key-Value pairs (Key, Value)
+                table.Add("ID", 507);
+                table.Add("Name", "John");
+                table.Add("Role", "Student");
+
+                Console.WriteLine("--- Hashtable Elements ---");
+                // Displaying items using DictionaryEntry
+                foreach (DictionaryEntry item in table)
+                {
+                    Console.WriteLine($"Key: {item.Key}, Value: {item.Value}");
+                }
+
+                // Accessing a specific value directly using its key
+                Console.WriteLine($"\nAccessing Name directly: {table["Name"]}");
+
+                Console.ReadLine();
+            }
+        }
+    }
+
+}
