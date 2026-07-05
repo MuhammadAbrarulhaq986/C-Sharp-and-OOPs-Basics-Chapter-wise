491306f0e7b566d0307d6e6285a4f09149e52246
Muhammad Abrar ul haq <ulhaqabrar106@gmail.com>
2026-07-06 00:35:48 +0500

    Generic Collections List in C# done

--- C# and OOPs Basics Chapter wise/32_Non-GenericCollectionsQueue.cs
+++ C# and OOPs Basics Chapter wise/32_Non-GenericCollectionsQueue.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿/*
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -36,3 +37,4 @@ namespace C__and_OOPs_Basics_Chapter_wise
         }
     }
 }
+ */
---
+++ C# and OOPs Basics Chapter wise/33_GenericCollectionsList.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__and_OOPs_Basics_Chapter_wise
+{
+    internal class _33_GenericCollectionsList
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                // Creating a type-safe list that strictly holds integers
+                List<int> numbers = new List<int>();
+
+                // Adding elements
+                numbers.Add(10);
+                numbers.Add(20);
+                numbers.Add(30);
+                // numbers.Add("Hello"); // ❌ Compile Error! Prevents accidental mixed-type inputs
+
+                Console.WriteLine("--- List<int> Elements ---");
+                foreach (int num in numbers)
+                {
+                    Console.WriteLine(num);
+                }
+
+                // Removing an element by value
+                numbers.Remove(20);
+                Console.WriteLine($"\nTotal elements left after removal: {numbers.Count}");
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
