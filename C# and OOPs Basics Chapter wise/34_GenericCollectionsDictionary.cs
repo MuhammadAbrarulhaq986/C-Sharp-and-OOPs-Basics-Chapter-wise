3e7e7026547238c73b3850930688615a1090c62d
Muhammad Abrar ul haq <ulhaqabrar106@gmail.com>
2026-07-07 11:56:45 +0500

    Generic Collections Dictionary <TKey, TValue> in C# done

---
+++ C# and OOPs Basics Chapter wise/34_GenericCollectionsDictionary.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__and_OOPs_Basics_Chapter_wise
+{
+    internal class _34_GenericCollectionsDictionary
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                // Key must be string, Value must be int
+                Dictionary<string, int> studentAges = new Dictionary<string, int>();
+
+                studentAges.Add("Alice", 21);
+                studentAges.Add("Bob", 22);
+
+                Console.WriteLine("--- Dictionary Elements ---");
+                // Iterating over KeyValuePair elements safely
+                foreach (KeyValuePair<string, int> kvp in studentAges)
+                {
+                    Console.WriteLine($"Student: {kvp.Key}, Age: {kvp.Value}");
+                }
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
