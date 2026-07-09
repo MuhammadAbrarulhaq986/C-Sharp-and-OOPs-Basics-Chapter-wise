075390cf2a593501b2140a1fb00e8a7271a4d476
Muhammad Abrar ul haq <ulhaqabrar106@gmail.com>
2026-07-09 13:14:12 +0500

    Custom Generic Implementations Creating a Generic Class: Generic Box Class using placeholder token <T>. done in C#

---
+++ C# and OOPs Basics Chapter wise/36_CustomGenericImplementationsCreatingAGenericClass.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__and_OOPs_Basics_Chapter_wise
+{
+    internal class _36_CustomGenericImplementationsCreatingAGenericClass
+    {
+        // Generic Box Class using placeholder token <T>
+        public class Box<T>
+        {
+            private T content;
+
+            public void SetContent(T item)
+            {
+                content = item;
+            }
+
+            public T GetContent()
+            {
+                return content;
+            }
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                // 1. Initializing Box specifically for storing integers
+                Box<int> intBox = new Box<int>();
+                intBox.SetContent(12345);
+                Console.WriteLine($"Integer Box Content: {intBox.GetContent()}");
+
+                // 2. Initializing the exact same class for string types instead
+                Box<string> stringBox = new Box<string>();
+                stringBox.SetContent("Learning Generics is fun!");
+                Console.WriteLine($"String Box Content: {stringBox.GetContent()}");
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
